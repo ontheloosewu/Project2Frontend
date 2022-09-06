9cb81b98b09db039b9e499045da065bb132f0f5c
jalec <jaleckc@gmail.com>
2022-09-06 14:26:02 -0700

    updated functionality

--- src/app/components/viewgradestable/viewgradestable.component.ts
+++ src/app/components/viewgradestable/viewgradestable.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Grade } from 'src/app/models/grade';
+import { Student } from 'src/app/models/student';
 import { GradeUtilService } from 'src/app/services/grade-util.service';
+import { StudentUtilService } from 'src/app/services/student-util.service';
 
 @Component({
   selector: 'app-viewgradestable',
@@ -9,11 +11,16 @@ import { GradeUtilService } from 'src/app/services/grade-util.service';
 })
 export class ViewgradestableComponent implements OnInit {
 
-  constructor(private gradeService: GradeUtilService) { }
+  constructor(private gradeService: GradeUtilService, private studentService:StudentUtilService) { }
 
   id:number = 0;
   grades:Grade[] = [];
+  students:Student[] = [];
   ngOnInit(): void {
+    (async () => {
+      this.students = await this.studentService.getAllStudents();
+    })();
+
   }
 
   async retrieveGrades(){
